f20f1f54a023b631b715f31dc10ff2230f233396
stefy <stefy@LAPTOP-S6BH1BE7>
2023-03-21 15:48:16 -0300

    mi primer prueba

---
+++ Clasegithub/src/Clasegithubprueba.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class Clasegithubprueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "prueba");
+	}
+
+}
