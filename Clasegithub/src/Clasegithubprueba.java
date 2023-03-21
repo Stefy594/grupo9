5688d7e9d23fab619a7fa127cf3185c96111f9f3
TorresNadia <nata-03-27@hotmail.com.ar>
2023-03-21 16:03:01 -0300

    Comentario interno

--- Clasegithub/src/Clasegithubprueba.java
+++ Clasegithub/src/Clasegithubprueba.java
@@ -5,6 +5,8 @@ public class Clasegithubprueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		JOptionPane.showMessageDialog(null, "prueba");
+		
+		JOptionPane.showMessageDialog(null, "Hola");
 	}
 
 }
